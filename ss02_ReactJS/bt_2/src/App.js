e6ddb4867cf9e8a8db3978e45fc66a3c23488175
vanhieu272 <hoangtranvanhieu@gmail.com>
2023-12-18 10:54:18 +0700

    hieu up bt ss02

---
+++ ss02_ReactJS/bt_2/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ProfileStudent from "./components/ProfileStudent";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <ProfileStudent></ProfileStudent>
+      </>
+    </div>
+  );
+}
+
+export default App;
---
+++ ss02_ReactJS/bt_3/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import SignIn from "./components/SignIn";
+
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn/>
+    </div>
+  );
+}
+
+export default App;
